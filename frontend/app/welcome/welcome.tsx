85950569882f16e519c9028c54a9c57a048dc27b
小渕周 <shukobuchi@gmail.com>
2025-05-26 18:52:07 +0900

    バックエンドからのresponseをフロントエンドに表示

--- frontend/app/welcome/welcome.tsx
+++ frontend/app/welcome/welcome.tsx
@@ -2,21 +2,46 @@ import { useState } from "react";
 
 export function Welcome() {
   const [input, setInput] = useState("");
+  const [answer, setAnswer] = useState("")
 
-  const onSend = () => {
-    fetch("http://localhost:8000/api/llm", {
+  const onSend = async () => {
+    setAnswer("")
+
+    const res = await fetch("http://localhost:8000/api/llm", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ query: input }),
     })
+    const data = await res.json()
+    setAnswer(data.answer)
   }
 
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <div>
+          <div>
+          <div>
+            <label htmlFor="answer">回答</label>
+          </div>
+          <div>
+            <textarea
+              id="answer"
+              rows={20}
+              cols={100}
+              value={answer}
+              readOnly
+              style={{
+                padding: "0.5rem",
+                border: "1px solid #ccc",
+                outline: "none",
+                boxShadow: "none",
+              }}
+            />
+          </div>
+        </div>
           <div>
             <label htmlFor="message">メッセージ</label>
           </div>
